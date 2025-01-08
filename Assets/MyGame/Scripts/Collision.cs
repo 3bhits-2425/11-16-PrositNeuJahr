6afd6f1485c2990818c71f2caae3fadeb929dd6f
VSPAJIC <viktor.spajic07@Htl-salzburg.ac.at>
2025-01-08 10:19:35 +0100

    Assets/

--- Assets/MyGame/Scripts/Collision.cs
+++ Assets/MyGame/Scripts/Collision.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 public class GiftCollision : MonoBehaviour
 {
     public GameObject rocketPrefab;
-    public string cameraTag = "MainCamera"; // Der Tag, über den die Kamera gefunden wird
+    public string cameraTag = "MainCamera"; 
     public float transitionSpeed = 5f;
-    public Vector3 targetPosition = new Vector3(-40f, 10f, -10f); // Zielposition der Kamera
+    public Vector3 targetPosition = new Vector3(-40f, 10f, -10f);  // Provisorische Position
     public float resetDelay = 4f;
 
     private Camera mainCamera;
     private Vector3 originalPosition;
-    private bool moveToTarget = false;
-    private bool moveToOriginal = false;
-    private float timer = 0f;
+    
+    
 
     private void Start()
     {
@@ -39,42 +38,13 @@ public class GiftCollision : MonoBehaviour
                 Instantiate(rocketPrefab, transform.position, Quaternion.identity);
             }
 
-            moveToTarget = true;
-            timer = 0f;
+           
+       
         }
     }
 
     private void Update()
     {
-        if (mainCamera == null) return;
-
-        if (moveToTarget)
-        {
-            mainCamera.transform.position = Vector3.Lerp(
-                mainCamera.transform.position, targetPosition, Time.deltaTime * transitionSpeed);
-
-            if (Vector3.Distance(mainCamera.transform.position, targetPosition) < 0.1f)
-            {
-                moveToTarget = false;
-                moveToOriginal = true;
-                timer = 0f;
-            }
-        }
-
-        if (moveToOriginal)
-        {
-            timer += Time.deltaTime;
-
-            if (timer >= resetDelay)
-            {
-                mainCamera.transform.position = Vector3.Lerp(
-                    mainCamera.transform.position, originalPosition, Time.deltaTime * transitionSpeed);
-
-                if (Vector3.Distance(mainCamera.transform.position, originalPosition) < 0.1f)
-                {
-                    moveToOriginal = false;
-                }
-            }
-        }
+        
     }
 }
